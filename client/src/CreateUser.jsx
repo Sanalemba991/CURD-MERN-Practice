5e6cf945e7b26730395c6cad29a61081952ac934
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-14 20:06:01 +0530

    Create

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function CreateUser() {
+  
   return (
     <div>
-        CreateUser
-      
+      <div>
+        <form>
+          <div>
+            <h2>Create User</h2>
+            <label htmlFor="">Name</label>
+            <input type="Text" placeholder="Name"></input>
+          </div>
+
+          <div>
+            <label htmlFor="">Email</label>
+            <input type="Text" placeholder="email"></input>
+          </div>
+          <div>
+            <label htmlFor="">Age</label>
+            <input type="Text" placeholder="age"></input>
+          </div>
+        <button >Submit</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateUser
+export default CreateUser;
