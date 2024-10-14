90726094e7fc8cea415a2f21db6f441f955ec938
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-14 19:50:13 +0530

    TABLE FOMAT

--- client/src/User.jsx
+++ client/src/User.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 function User() {
   return (
     <div>
-      User
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Email
+                    </th>
+                    <th>
+                        Age
+                    </th>
+                    <th>
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    </div>
     </div>
   )
 }
