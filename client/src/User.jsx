e272315a1fcb1f121887dfc20680ac2ffc3644a1
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-14 20:01:05 +0530

    Giving Link for Craeate

--- client/src/User.jsx
+++ client/src/User.jsx
@@ -1,51 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function User() {
-    const [users,setUsers]=useState([{    
-        Name:"Sanalemba",
-        Email:"laitonjam99@gmail.com",
-        Age:29}
-    
-    ])
+  const [users, setUsers] = useState([
+    {
+      Name: "Sanalemba",
+      Email: "laitonjam99@gmail.com",
+      Age: 29,
+    },
+  ]);
+
   return (
     <div>
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th>
-                        Name
-                    </th>
-                    <th>
-                        Email
-                    </th>
-                    <th>
-                        Age
-                    </th>
-                    <th>
-                        Action
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    users.map((user)=>{
-                        return
-                        <tr>    <td>{user.Name}</td>
-                        <td>{user.Email}</td>
-                        <td>{user.Age}</td>
-                        <td><button>Edit</button>
-                        <button>Delete</button></td>
-                   </tr>
-                    
-                    }
-                )}
-
-            </tbody>
-        </table>
+        <Link to="/create">Add+</Link>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr>
+              <td>{user.Name}</td>
+              <td>{user.Email}</td>
+              <td>{user.Age}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-    </div>
-  )
+  );
 }
 
-export default User
+export default User;
