4baf955dcd49d56407b9d63f9a252711608d1ac5
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-15 21:16:29 +0530

    making Interact with Formtend and backend

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateUser() {
+
+  const [name,setName]=useState()
+  
+  const [email,setEmail]=useState()
+  const [age,setAge]=useState()
+
   
   return (
     <div>
       <div>
-        <form>
+        <form >
           <div>
             <h2>Create User</h2>
             <label htmlFor="">Name</label>
-            <input type="Text" placeholder="Name"></input>
+            <input type="Text" placeholder="Name"
+            onChange={(e)=>setName(e.target.value)}></input>
+          
           </div>
 
           <div>
             <label htmlFor="">Email</label>
-            <input type="Text" placeholder="email"></input>
+            <input type="Text" placeholder="email"
+             onChange={(e)=>setEmail(e.target.value)}></input>
           </div>
           <div>
             <label htmlFor="">Age</label>
-            <input type="Text" placeholder="age"></input>
+            <input type="Text" placeholder="age"
+             onChange={(e)=>setAge(e.target.value)}></input>
           </div>
         <button >Update</button>
         </form>
--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 // Import the cors module to enable Cross-Origin Resource Sharing
 const cors = require('cors'); // Fixed the typo here
 
+
+const UserModel=require('./models/User')
 // Create an instance of an Express application
 const app = express();
 
@@ -18,7 +20,13 @@ app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:/curd")
 
-
+app.post("/create",(req,res)=>{
+    UserModel.create(req.body)
+    .then(user=>{
+        res.json((user))
+        .catch(err=>res.json(err))
+    })
+})
 // Start the server on port 3001 and log a message to the console
 app.listen(3001, () => {
     console.log("Server is Okay");
